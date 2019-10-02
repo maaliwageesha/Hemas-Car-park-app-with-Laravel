49ce3ddf891829ff8c1e6a1ddaae58c4f6862fab
Dulmini <chuta@1234>
2019-10-02 23:25:51 +0530

    commenting done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 /*
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------------------------------------
 | Model Factories
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------------------------------
 |
 | This directory should contain each of the model factory definitions for
 | your application. Factories provide a convenient way to generate new
