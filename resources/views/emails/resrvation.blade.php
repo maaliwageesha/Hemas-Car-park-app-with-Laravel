a798b607bd5dee4e7668bb15ed2c42a8b5c46eef
kaniya24 <dkanishka24@gmail.com>
2019-10-04 03:29:47 +0530

    modified the reservation. added missed slot id to the mail

--- resources/views/emails/resrvation.blade.php
+++ resources/views/emails/resrvation.blade.php
@@ -2,7 +2,8 @@
 # Your Reservation Code
 
 <strong>Your Email: </strong> {{ $data['email'] }} <br>
-<strong>Your Mobile Number: </strong> {{ $data['phone_number'] }} <br><br>
+<strong>Your Mobile Number: </strong> {{ $data['phone_number'] }} <br>
+<strong>Your Slot No: </strong> {{ $data['slot_id'] }} <br><br>
 
 Hi {{ explode(' ', $data['name'])[0] }}! your reservation code is <h1 class="text-bold">{{ $data['reservation_code'] }}</h1>
 
