ed65bf21dc5ac21c3337d87219da5bfb6d62b7fe
kaniya24 <dkanishka24@gmail.com>
2019-10-04 03:27:43 +0530

    reservation.blade.php file added please add the messeges.blade file

---
+++ resources/views/emails/resrvation.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Your Reservation Code
+
+<strong>Your Email: </strong> {{ $data['email'] }} <br>
+<strong>Your Mobile Number: </strong> {{ $data['phone_number'] }} <br><br>
+
+Hi {{ explode(' ', $data['name'])[0] }}! your reservation code is <h1 class="text-bold">{{ $data['reservation_code'] }}</h1>
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
