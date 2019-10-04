94e1d38e9afec436581d61e845b50ca8a135f749
maaliwageesha <maaliwageesha26@yahoo.com>
2019-10-04 13:26:53 +0530

    complaint controller  added

---
+++ app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\complaint;
+
+class ComplaintController extends Controller
+{
+    public function index()
+    {
+        return Complaint::latest()->get();
+    }
+
+    public function store(Request $request)
+    {
+        $validated_data = $this->validate($request, [
+            'name' => 'required',
+            'email' => 'email|required|max:155',
+            'complaint' => 'required|min:5',
+        ]);
+        Complaint::create($validated_data);
+        return ['status' => 200, 'message' => 'success'];
+    }
+
+    public function destroy($id)
+    {
+        $complaint = Complaint::findOrFail($id);
+        $complaint->delete();
+        return ['message' => 'Complaint deleted successfully!'];
+    }
+}
